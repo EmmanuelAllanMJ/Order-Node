577b38b79a0bd7227d4919cb4606167b4f63b55b
Emmanuel Allan <90669430+EmmanuelAllanMJ@users.noreply.github.com>
2022-12-17 17:01:28 +0530

    Deleted products

--- controllers/admin.js
+++ controllers/admin.js
@@ -77,6 +77,12 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect("/admin/products");
+  Product.deleteById(prodId)
+    .then(() => {
+      console.log("Deleted succesfully");
+      res.redirect("/admin/products");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
--- models/product.js
+++ models/product.js
@@ -7,7 +7,7 @@ module.exports = class Product {
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
-    this._id = new mongodb.ObjectId(id);
+    this._id = id ? new mongodb.ObjectId(id) : null;
   }
 
   save() {
@@ -21,7 +21,7 @@ module.exports = class Product {
         .collection("products")
         .updateOne({ _id: this._id }, { $set: this });
     } else {
-      let dbOp = db.collection("products").insertOne(this);
+      dbOp = db.collection("products").insertOne(this);
     }
     return dbOp
       .then((result) => {
@@ -58,5 +58,18 @@ module.exports = class Product {
         return product;
       })
       .catch((err) => console.log(err));
+  }
+
+  static deleteById(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then((result) => {
+        console.log("Deleted");
+      })
+      .catch((err) => {
+        console.log("Deleted");
+      });
   }
 };
