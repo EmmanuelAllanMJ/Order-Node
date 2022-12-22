610eb72bfedc7c6ace5ca0f50fd8e78492c13107
Emmanuel Allan <90669430+EmmanuelAllanMJ@users.noreply.github.com>
2022-12-22 15:38:12 +0530

    Added user schema

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 // db
-// const User = require("./models/user");
+const User = require("./models/user");
 
 // Importing routes
 const adminRoutes = require("./routes/admin");
@@ -24,15 +24,15 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // This middleware is to find the user and then we store it in user in our request
 // This is just to extract userId and use it in all our app
-// app.use((req, res, next) => {
-//   User.findById("63a154107b37bc642b17cba3")
-//     .then((user) => {
-//       // Creating a object for user
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch((err) => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById("63a42c5810c419781a2f1d7a")
+    .then((user) => {
+      // Creating a object for user
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 // To use them like calling a function
 app.use("/admin", adminRoutes);
@@ -45,6 +45,16 @@ mongoose
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.lfr5p.mongodb.net/${process.env.MONGODB_DEFAULT_DB}?retryWrites=true&w=majority`
   )
   .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Allan",
+          email: "allan@test.com",
+          cart: { items: [] },
+        });
+        user.save();
+      }
+    });
     app.listen(3000);
   })
   .catch((err) => {
