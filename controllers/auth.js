a62afb1ae2c6d88123c56d8f89efdfb766830f55
Emmanuel Allan <90669430+EmmanuelAllanMJ@users.noreply.github.com>
2023-01-03 12:21:30 +0530

    Creating token for reseting password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+// inbuilt lib
+const crypto = require("crypto");
 const bcrypt = require("bcryptjs");
 const nodemailer = require("nodemailer");
 
@@ -178,5 +180,125 @@ exports.postLogout = (req, res, next) => {
   req.session.destroy((err) => {
     console.log(err);
     res.redirect("/");
+  });
+};
+
+exports.getReset = (req, res, next) => {
+  res.render("auth/reset", {
+    pageTitle: "Reset Password",
+    path: "/reset",
+    errorMessage: req.flash("error")[0],
+  });
+};
+exports.postReset = (req, res, next) => {
+  // 32 random bytes, this gives us a callback
+  crypto.randomBytes(32, (err, buff) => {
+    if (err) {
+      console.log(err);
+      return res.redirect("/");
+    }
+    // creating token
+    const token = buff.toString("hex");
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        if (!user) {
+          req.flash("error", "No account with the email is found.");
+          return res.redirect("/reset");
+        }
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600000;
+        return user.save();
+      })
+      .then((result) => {
+        res.redirect("/");
+        const htmlContent = `
+            <!doctype html>
+            <html lang="en-US">
+
+            <head>
+                <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+                <title>Reset Password Email Template</title>
+                <meta name="description" content="Reset Password Email Template.">
+                <style type="text/css">
+                    a:hover {text-decoration: underline !important;}
+                </style>
+            </head>
+
+            <body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
+                <!--100% body table-->
+                <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
+                    style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+                    <tr>
+                        <td>
+                            <table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;" width="100%" border="0"
+                                align="center" cellpadding="0" cellspacing="0">
+                                <tr>
+                                    <td style="height:80px;">&nbsp;</td>
+                                </tr>
+                                
+                                <tr>
+                                    <td style="height:20px;">&nbsp;</td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
+                                            style="max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
+                                            <tr>
+                                                <td style="height:40px;">&nbsp;</td>
+                                            </tr>
+                                            <tr>
+                                                <td style="padding:0 35px;">
+                                                    <h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">You have
+                                                        requested to reset your password</h1>
+                                                    <span
+                                                        style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
+                                                    <p style="color:#455056; font-size:15px;line-height:24px; margin:0;">
+                                                        We cannot simply send you your old password. A unique link to reset your
+                                                        password has been generated for you. To reset your password, click the
+                                                        following link and follow the instructions.
+                                                    </p>
+                                                    <a href="http://localhost:3000/reset/${token}"
+                                                        style="background:#00695c;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">Reset
+                                                        Password</a>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td style="height:40px;">&nbsp;</td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                <tr>
+                                    <td style="height:20px;">&nbsp;</td>
+                                </tr>
+                              
+                                <tr>
+                                    <td style="height:80px;">&nbsp;</td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+                <!--/100% body table-->
+            </body>
+
+            </html>`;
+        const msg = {
+          to: req.body.email,
+          from: `pixiemj00@gmail.com`,
+          subject: "Password Reset",
+          html: htmlContent,
+        };
+        //send the email
+        transporter.sendMail(msg, function (error, info) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log("Email sent: " + info.response);
+          }
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   });
 };
