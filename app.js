f3cbd258c0e3a2e48a258e6a63e34e5d9a824968
Emmanuel Allan <90669430+EmmanuelAllanMJ@users.noreply.github.com>
2023-03-28 23:38:50 +0530

    Minor change

--- app.js
+++ app.js
@@ -106,8 +106,7 @@ app.use(
       maxAge: 1000 * 60 * 60 * 7,
       saveUninitialized: true,
       store: store,
-    }
-    // ({ _ts: 1 }, { expireAfterSeconds: 60 })
+    }({ _ts: 1 }, { expireAfterSeconds: 60 })
   )
 );
 // using csrf middleware
